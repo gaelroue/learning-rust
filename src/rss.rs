b76bf2228bf81ee6b7cdfdfff9e7e02dafcfcae7
Gael Roue <roue.gael3@gmail.com>
2016-07-18 22:38:16 +0200

    Prepare rss parsing

--- src/rss.rs
+++ src/rss.rs
@@ -1,3 +1,11 @@
+pub struct Post {
+    pub author: String,
+    pub title: String,
+    pub link: String,
+    pub description: String,
+    pub guid: String,
+}
+
 pub fn rss_parse(data: &Vec<u8>) {
     let content = String::from_utf8_lossy(data);
     println!("{}", content);
