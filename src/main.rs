b87e373c6810ae233cde8dbcfc4105f5874d530f
Gael Roue <roue.gael3@gmail.com>
2016-07-24 22:08:29 +0200

    Basic rss parser

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,39 @@
-//extern crate curl;
+extern crate curl;
+extern crate regex;
 
-//use curl::easy::Easy;
+use curl::easy::Easy;
 use std::path::Path;
 
-//mod rss;
+mod rss;
 mod ini;
 
 fn main() {
-    let rss_test = "
-<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
-<rss version=\"2.0\">
-    <channel>
-        <title>RSS Title</title>
-        <description>This is an example of an RSS feed</description>
-        <link>http://www.example.com/main.html</link>
-        <lastBuildDate>Mon, 06 Sep 2010 00:01:00 +0000 </lastBuildDate>
-        <pubDate>Sun, 06 Sep 2009 16:20:00 +0000</pubDate>
-        <ttl>1800</ttl>
-
-        <item>
-            <title>Example entry</title>
-            <description>Here is some text containing an interesting description.</description>
-            <link>http://www.example.com/blog/post/1</link>
-            <guid isPermaLink=\"true\">7bd204c6-1655-4c27-aeee-53f933c5395f</guid>
-            <pubDate>Sun, 06 Sep 2009 16:20:00 +0000</pubDate>
-        </item>
-    </channel>
-</rss>
-";
-
     let path = Path::new("feed.ini");
 
     let feed = ini::Section::new("Feed", vec!["name", "url"]);
 
     let rss_list = feed.load_conf(&path);
+
+    let mut handle = Easy::new();
+    let mut data = Vec::new();
+
     for (rss, url) in &rss_list {
-        println!("{} : {}", rss, url);
+        println!("-------------------- {} -----------------", rss);
+        handle.url(url).unwrap();
+        {
+            let mut transfer = handle.transfer();
+            transfer.write_function(|buf| {
+                data.extend_from_slice(buf);
+                Ok(buf.len())
+            }).unwrap();
+            transfer.perform().unwrap();
+        }
+
+        let posts = rss::rss_parse(&data);
+
+        for post in posts.iter() {
+            println!("{} -> {}", post.title, post.link);
+        }
     }
 
-    //let mut handle = Easy::new();
-    //let mut data = Vec::new();
-    //handle.url("https://news.ycombinator.com/rss").unwrap();
-    //{
-    //    let mut transfer = handle.transfer();
-    //    transfer.write_function(|buf| {
-    //        data.extend_from_slice(buf);
-    //        Ok(buf.len())
-    //    }).unwrap();
-    //    transfer.perform().unwrap();
-    //}
-    //rss::rss_parse(&data);
 }
--- src/rss.rs
+++ src/rss.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 pub struct Post {
     pub author: String,
     pub title: String,
@@ -6,7 +8,44 @@ pub struct Post {
     pub guid: String,
 }
 
-pub fn rss_parse(data: &Vec<u8>) {
-    let content = String::from_utf8_lossy(data);
-    println!("{}", content);
+fn xml_parse(data: String, tag: &str) -> String {
+    let pattern = format!(r"(<{}>)(.+?)(</{}>)", tag, tag);
+    let re = match Regex::new(&pattern) {
+        Ok(re) => re,
+        Err(err) => panic!("{}", err),
+    };
+
+    match re.captures(&data) {
+        Some(cap) => cap.at(2).unwrap_or("").to_string(),
+        _ => "".to_string()
+    }
+}
+
+pub fn rss_parse(data: &Vec<u8>) -> Vec<Post> {
+    let content = String::from_utf8_lossy(data).into_owned();
+
+    let mut posts : Vec<Post> = vec![];
+
+    let re = Regex::new(r"<item>[\s\S]*?</item>").unwrap();
+
+    for cap in re.captures_iter(&content) {
+        let item = match cap.at(0) {
+            Some(x) => x,
+            _ => ""
+        };
+        if item == "" {
+            continue;
+        }
+
+        let next_post = Post{
+            author: xml_parse(item.to_string(), "author"),
+            title: xml_parse(item.to_string(), "title"),
+            link: xml_parse(item.to_string(), "link"),
+            description: xml_parse(item.to_string(), "description"),
+            guid: xml_parse(item.to_string(), "guid"),
+        };
+
+        posts.push(next_post);
+    }
+    posts
 }
